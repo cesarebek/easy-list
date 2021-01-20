aef4ea945b6f681cbd3401fe00a1ec7408d2effb
Cesare Bek <cesarebek1@gmail.com>
2021-01-20 22:29:04 +0100

    1. Project Setup

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework.js';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    //Here goes the customization code for vuetify classes
+  },
+});
